20253812c116c6bfa1b351097feec27c524e3d22
Wangmafei <wangmafei@gmail.com>
2016-05-23 21:09:36 +0800

    no message

---
+++ OSKit/OSKit/OSMath.h
@@ -0,0 +1,13 @@
+//
+//  OSMath.h
+//  OSKit
+//
+//  Created by Wangmafei on 16/5/23.
+//  Copyright © 2016年 Wangmafei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSMath : NSObject
+
+@end
---
+++ OSKit/OSKit/OSMath.m
@@ -0,0 +1,13 @@
+//
+//  OSMath.m
+//  OSKit
+//
+//  Created by Wangmafei on 16/5/23.
+//  Copyright © 2016年 Wangmafei. All rights reserved.
+//
+
+#import "OSMath.h"
+
+@implementation OSMath
+
+@end
